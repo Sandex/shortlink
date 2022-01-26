a731709ab98203d42036a1668cd3f8158093c8b1
Alexander Bezruchenko <bezru@outlook.com>
2022-01-26 03:14:09 +0300

    increment_7

--- internal/config/Config.go
+++ internal/config/Config.go
@@ -3,5 +3,5 @@ package config
 type Config struct {
 	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"127.0.0.1:8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"/api/shorten"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"./store"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"/tmp/store.dat"`
 }
